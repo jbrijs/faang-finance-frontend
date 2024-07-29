7f21d5adc1559258d7480a842f2a5735b12aaacd
jbrijs <joebrijs@gmail.com>
2024-07-28 19:12:20 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import { ThemeProvider } from './components/theme-provider'
+import Navbar from './components/navbar'
+
+
+
+function App() {
+
+
+  return (
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
+        <Navbar/>
+    </ThemeProvider>
+  )
+}
+
+export default App
---
+++ src/components/navbar.tsx
@@ -0,0 +1,36 @@
+import {
+    Menubar,
+    MenubarContent,
+    MenubarItem,
+    MenubarMenu,
+    MenubarSeparator,
+    MenubarShortcut,
+    MenubarTrigger,
+  } from "@/components/ui/menubar"
+
+  import React from 'react'
+import { ModeToggle } from "./mode-toggle"
+  
+  function navbar() {
+    return(
+        <Menubar className="rounded-none border-x-0">
+        <MenubarMenu>
+            <ModeToggle/>
+          <MenubarTrigger>File</MenubarTrigger>
+          <MenubarContent>
+            <MenubarItem>
+              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
+            </MenubarItem>
+            <MenubarItem>New Window</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Share</MenubarItem>
+            <MenubarSeparator />
+            <MenubarItem>Print</MenubarItem>
+          </MenubarContent>
+        </MenubarMenu>
+      </Menubar>
+      
+    )
+  }
+  
+  export default navbar
