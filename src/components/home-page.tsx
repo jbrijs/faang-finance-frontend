1fddca0241df732dd34369ae1a360d29964ec92e
jbrijs <joebrijs@gmail.com>
2024-07-28 21:49:36 -0600

    nav bar

--- src/components/home-page.tsx
+++ src/components/home-page.tsx
@@ -5,16 +5,19 @@ import { testData } from "../utils";
 export function HomePage() {
   return (
     <>
-      <div className="flex flex-row flex-wrap items-center justify-center gap-4 pt-10">
-        {testData.map((data, index) => (
-          <PredictionCard
-            key={index}
-            ticker={data.ticker}
-            companyName={data.companyName}
-            prediction={data.prediction}
-            previous_close={data.previous_close}
-          />
-        ))}
+      <div className="m-8">
+        <h1 className="text-4xl">Todays Predictions</h1>
+        <div className="flex flex-row flex-wrap items-center justify-center gap-4 pt-10">
+          {testData.map((data, index) => (
+            <PredictionCard
+              key={index}
+              ticker={data.ticker}
+              companyName={data.companyName}
+              prediction={data.prediction}
+              previous_close={data.previous_close}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,36 +1,75 @@
 import {
-    Menubar,
-    MenubarContent,
-    MenubarItem,
-    MenubarMenu,
-    MenubarSeparator,
-    MenubarShortcut,
-    MenubarTrigger,
-  } from "@/components/ui/menubar"
+  Menubar,
+  MenubarContent,
+  MenubarItem,
+  MenubarMenu,
+  MenubarSeparator,
+  MenubarShortcut,
+  MenubarTrigger,
+} from "@/components/ui/menubar";
 
-  import React from 'react'
-import { ModeToggle } from "./mode-toggle"
-  
-  function navbar() {
-    return(
-        <Menubar className="rounded-none border-x-0">
-        <MenubarMenu>
-            <ModeToggle/>
-          <MenubarTrigger>File</MenubarTrigger>
-          <MenubarContent>
-            <MenubarItem>
-              New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-            </MenubarItem>
-            <MenubarItem>New Window</MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem>Share</MenubarItem>
-            <MenubarSeparator />
-            <MenubarItem>Print</MenubarItem>
-          </MenubarContent>
-        </MenubarMenu>
-      </Menubar>
-      
-    )
-  }
-  
-  export default navbar
+import React from "react";
+import { ModeToggle } from "./mode-toggle";
+
+const companies = [
+  "Adobe",
+  "Amazon",
+  "Apple",
+  "Google",
+  "Meta",
+  "Microsoft",
+  "Netflix",
+  "Nvidia",
+];
+
+function navbar() {
+  return (
+    <Menubar className="rounded-none border-x-0 h-14">
+      <div className="flex flex-row justify-between w-full mx-10">
+        <div className="flex flex-row gap-10">
+          <MenubarMenu>
+            <MenubarTrigger className="text-md">Predictions</MenubarTrigger>
+            <MenubarContent>
+              {companies.map((company) => (
+                <MenubarItem key={company}>{company}</MenubarItem>
+              ))}
+              <MenubarSeparator/>
+              <MenubarItem>
+                All Companies
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger className="text-md">
+              Project Description
+            </MenubarTrigger>
+            <MenubarContent>
+            {companies.map((company) => (
+                <MenubarItem key={company}>{company}</MenubarItem>
+              ))}
+              <MenubarSeparator/>
+              <MenubarItem>
+                All Companies
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+          <MenubarMenu>
+            <MenubarTrigger className="text-md">Historical Data</MenubarTrigger>
+            <MenubarContent>
+            {companies.map((company) => (
+                <MenubarItem key={company}>{company}</MenubarItem>
+              ))}
+              <MenubarSeparator/>
+              <MenubarItem>
+                All Companies
+              </MenubarItem>
+            </MenubarContent>
+          </MenubarMenu>
+        </div>
+        <ModeToggle />
+      </div>
+    </Menubar>
+  );
+}
+
+export default navbar;
