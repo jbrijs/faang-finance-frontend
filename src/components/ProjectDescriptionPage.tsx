497a5a73ae468409312eb8140ab26f718661fcc4
jbrijs <joebrijs@gmail.com>
2024-10-05 15:04:37 -0600

    collabpsible

--- src/components/ProjectDescriptionPage.tsx
+++ src/components/ProjectDescriptionPage.tsx
@@ -1,10 +1,19 @@
+import architectureImage from "../assets/architecture.png";
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from "./ui/collapsible";
+
 const ProjectDescriptionPage = () => {
   return (
-    <div className="flex flex-row items-center justify-center w-full overflow-y-auto">
+    <div className="flex flex-col items-center justify-center w-full overflow-y-auto">
+      <h1 className="text-4xl font-semibold py-10">Project Description</h1>
       <div className="p-10 flex flex-col items-center justify-center w-fit">
-        <h1 className="text-3xl font-medium pb-4">Project Description</h1>
         <div className="w-full flex flex-row justify-start pb-2">
-          <h2 className="text-2xl font-medium">Project Goals</h2>
+          <h2 className="text-3xl text-muted-foreground font-medium">
+            Project Goals
+          </h2>
         </div>
         <p className="max-w-prose text-lg pb-8">
           The goal of this project was to train a number of Artificial Neural
@@ -14,18 +23,79 @@ const ProjectDescriptionPage = () => {
           ADVICE.
         </p>
         <div className="w-full flex flex-row justify-start pb-2">
-          <h2 className="text-2xl font-medium">Project Iteration</h2>
+          <h2 className="text-3xl text-muted-foreground font-medium">
+            Idea Iteration
+          </h2>
         </div>
         <p className="max-w-prose text-lg">
           The original vision of the project was to use Flask, a lightweight web
           framework in Python, to load the models and make predictions. The
           Flask app would be running on an AWS EC2 instance and the models and
           data would be stored in AWS S3. Having a persistant server soon proved
-          to be the wrong tool for due to a few reasons. First, getting an instance with
-          enough compute for model inference proved to be costly, and second,
-          predictions would only need to be made once a day and could be stored
-          instead of a prediction being made everytime a user landed on the web
-          page.
+          to be the wrong tool for the job for a few reasons. First, getting an
+          instance with enough compute for model inference proved to be costly,
+          and second, predictions would only need to be made once a day and
+          could be stored instead of a prediction being made everytime a user
+          landed on the web page. Instead, I used an AWS Lambda Function that
+          would be called after market close to make predictions for the next
+          trading day. The whole architecture can be seen in Figure 1 below.
+        </p>
+      </div>
+      <div className="md:w-5/6 lg:w-1/2 md:p-10 w-full p-4">
+        <img src={architectureImage} alt="AWS cloud architecture picture" />
+        <span className="flex flex-row gap-1.5 text-muted-foreground">
+          <p>Figure 1:</p>
+          <p className="italic">FAANG Finance AWS Cloud Architecture</p>
+        </span>
+      </div>
+      <div className="p-10 flex flex-col items-center justify-center w-fit">
+        <div className="w-full flex flex-row justify-start pb-2">
+          <h2 className="text-3xl text-muted-foreground font-medium">
+            Methods Used
+          </h2>
+        </div>
+        <div className="w-full flex flex-row justify-start pb-2">
+          <h3 className="text-xl text-muted-foreground font-bold">
+            Data Fetching
+          </h3>
+        </div>
+        <p className="max-w-prose text-lg pb-8">
+          I used the Alpha Vantage API to gather stock price data. The API call
+          I used returned <code>time_stamp</code>, <code>open</code>,{" "}
+          <code>close</code>, and <code>volume</code> datapoints for as long as
+          the company has been publicly traded. With this data, I was able to
+          engineer many other features that would aid in model training.
+        </p>
+        <Collapsible className="flex flex-col items-start justify-start text-left w-full">
+          <CollapsibleTrigger className="w-full flex flex-row justify-start pb-2">
+            <h3 className="text-xl text-left text-muted-foreground font-bold">
+              Feature Engineering
+            </h3>
+          </CollapsibleTrigger>
+          <CollapsibleContent>
+            <p className="max-w-prose text-lg pb-8">
+              I calculated many techincal indicators to provide my features for
+              training models. I calculated
+            </p>
+          </CollapsibleContent>
+        </Collapsible>
+        <div className="w-full flex flex-row justify-start pb-2">
+          <h2 className="text-3xl text-muted-foreground font-medium">
+            Idea Iteration
+          </h2>
+        </div>
+        <p className="max-w-prose text-lg">
+          The original vision of the project was to use Flask, a lightweight web
+          framework in Python, to load the models and make predictions. The
+          Flask app would be running on an AWS EC2 instance and the models and
+          data would be stored in AWS S3. Having a persistant server soon proved
+          to be the wrong tool for the job for a few reasons. First, getting an
+          instance with enough compute for model inference proved to be costly,
+          and second, predictions would only need to be made once a day and
+          could be stored instead of a prediction being made everytime a user
+          landed on the web page. Instead, I used an AWS Lambda Function that
+          would be called after market close to make predictions for the next
+          trading day. The whole architecture can be seen in Figure 1 below.
         </p>
       </div>
     </div>
